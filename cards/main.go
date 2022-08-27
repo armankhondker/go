6d3735b7d943d3a2915d6357ba3ffa5aa33ea7d8
Arman <armankhondker@utexas.edu>
2022-08-27 08:00:06 -0500

    update main.go

--- cards/main.go
+++ cards/main.go
@@ -3,6 +3,15 @@ package main
 import "fmt"
 
 func main() {
+	card := newCard()
+	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Ace of Spaces"
+}
+
+func testing() {
 	var card string = "Ace of Spaces" //don't need to write out string here necessarily, go can infer
 	var x int = 10
 	y := 22 //go infers the type of int
