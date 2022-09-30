692a511ba7c9bd4abfbe10d52d8f9a82f1250d51
Arman <armankhondker@utexas.edu>
2022-09-30 12:40:02 -0500

    Reciever funcs

--- cards/main.go
+++ cards/main.go
@@ -1,5 +1,23 @@
 package main
 
 func main() {
+	// var card string = "Aces of Spades"
+	// fmt.Println(card)
+	// card1 := "Aces of Diamonds"
+	// fmt.Println(card1)
+	// card1 = "Five of Diamonds"
+	// fmt.Println(card1)
+	// fmt.Println(helper())
+	// tester := helper()
+	// fmt.Println(tester)
+	// cards := []string{"Arman", "Ganiyu", "Punit"}
+	// cards = append(cards, "Test")
+	// fmt.Println(cards)
 
+	mycards := deck{"Kai", "Lindsey", "Jianfeng"}
+	mycards.print()
+}
+
+func helper() string {
+	return "Five of Diamonds"
 }
